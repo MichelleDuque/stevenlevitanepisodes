193c2e68804e032997340c707e06d3e0ff48c6d4
MichelleDuque <michelled_1994@hotmail.com>
2022-04-06 03:25:34 -0500

    quitar quotes

--- script.js
+++ script.js
@@ -331,7 +331,7 @@ const quotes = [
 
 function randomQuote() {
   let random = quotes[Math.floor(Math.random() * quotes.length)];
-  quotation.innerText = `“${random.quote}.”`;
+  quotation.innerText = `${random.quote}.`;
   source.innerText = random.source;
 }
 
